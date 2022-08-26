55e47d27ca1b91c31a6e0a3371495cdc4b9c4d7b
shvkmr536 <shvkmr536@gmail.com>
2022-08-26 17:46:15 +0530

    updated

---
+++ sql/sit/V2__Create_new_table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE demo (
+	id SERIAL PRIMARY KEY,
+	url VARCHAR(255) NOT NULL,
+	name VARCHAR(255) NOT NULL,
+	description VARCHAR (255),
+    last_update DATE
+);
+
+INSERT INTO demo (url, name)
+VALUES('https://www.postgresqltutorial.com','PostgreSQL Tutorial');
